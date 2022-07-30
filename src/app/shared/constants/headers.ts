353e56660e1858b72318bbb84f88bb6fd2daabc4
Anderson M <foxvc22@gmail.com>
2022-07-30 20:25:55 +0200

    [FIX][3007] Ajuste em politica CORS

--- src/app/shared/constants/headers.ts
+++ src/app/shared/constants/headers.ts
@@ -5,7 +5,8 @@ export const api:string = "https://apivieiraveiculos.herokuapp.com";
 
 export const defaultHeaders:HttpHeaders = new HttpHeaders({
     'content-type': 'application/json',
-    'Access-Control-Allow-Headers': ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept"],
+    'Access-Control-Allow-Headers':'*',
+    // 'Access-Control-Allow-Headers': ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept"],
     'Access-Control-Allow-Methods': ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     'Access-Control-Allow-Origin': api
 });
